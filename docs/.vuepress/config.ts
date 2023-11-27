9151aeeef8ceafa5d48a452dcf6d913cc02be53a
AhJindeg <AhJindeg@163.com>
2023-11-27 18:09:35 +0800

    ✨ feat: update dist

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,9 +15,30 @@ export default defineUserConfig({
     lastUpdatedText: '',
     editLink: false,
     navbar: [
-      { text: 'Home', link: '/' },
-      { text: 'Categories', link: '/categories/category1/1/' },
-      { text: 'Tags', link: '/tags/tag1/1/' },
+      { text: '主页', link: '/' },
+      {
+        text: '工具推荐',
+        children: [
+          {
+            text: 'VScode',
+            children: [
+              { text: '配置', link: '/tags/vscode/config/1/' },
+              { text: '插件', link: '/tags/vscode/plugin/1/' },
+            ],
+          },
+        ],
+      },
+      {
+        text: '技术分享',
+        children: [
+          { text: 'JavaScript', link: '/categories/javascript/1/' },
+          { text: 'TypeScript', link: '/categories/typescript/1/' },
+          { text: 'Vue', link: '/categories/vue/vue3/1/' },
+        ],
+      },
+      { text: '框架文档', link: '/docs/frame-document/' },
+      { text: '英文学习', link: '/docs/english-learning/' },
+      { text: '拳击训练', link: '/docs/boxing-training/' },
       {
         text: 'Docs',
         children: [
---
+++ docs/blogs/technology-sharing/vue/vue3/关于在VUE3中样式穿透的小建议.md
@@ -0,0 +1,14 @@
+---
+title: 关于在 VUE3 中样式穿透的小建议
+date: 2023/11/27
+categories:
+  - vue/vue3
+---
+
+使用 VUE3 项目后会因为使用的 CSS 预处理 不同导致`/deep/ & v-deep & >>>` 无效或者报错
+<br />
+可以修改为使用 `:deep(.className)`
+<br />
+设置`全局样式`使用 `:global(.className)`
+<br />
+设置`插槽样式`使用 `:slotted(.className)`
