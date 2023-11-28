2978e9664777bf8821da55b979428994d49427df
AhJindeg <AhJindeg@163.com>
2023-11-28 17:08:47 +0800

    ✨ feat: update dist

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -2,21 +2,39 @@ import { defineUserConfig } from 'vuepress'
 import recoTheme from 'vuepress-theme-reco'
 
 export default defineUserConfig({
+  // debug: true,
+  port: 8848,
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
   title: "AhJindeg's Blog",
-  description: '',
+  description: "AhJindeg's Blog, AhJindeg, Arthur, Arthur AJ. W., 阿烬°, 前端技术博客",
   theme: recoTheme({
-    home: '/',
     style: '@vuepress-reco/style-default',
-    logo: '/logo.png',
     author: 'AhJindeg',
     authorAvatar: '/head.png',
+    home: '/',
+    logo: '/logo.png',
     docsRepo: 'https://github.com/AhJindeg/AhJindeg.github.io.git',
-    docsBranch: 'main',
-    docsDir: 'example',
+    docsBranch: 'master',
+    docsDir: '',
     lastUpdatedText: '',
     editLink: false,
     navbar: [
-      { text: '主页', link: '/' },
+      {
+        text: '主页',
+        children: [
+          { text: '首页', link: '/' },
+          { text: '时间轴', link: '/timeline' },
+          { text: '博客列表', link: '/posts' },
+          { text: '友情链接', link: '/friendship-link' },
+          {
+            text: 'reco-docs',
+            children: [
+              { text: 'vuepress-reco', link: '/docs/theme-reco/home' },
+              { text: 'vuepress-theme-reco', link: '/blogs/other/guide' },
+            ],
+          },
+        ],
+      },
       {
         text: '工具推荐',
         children: [
@@ -40,13 +58,6 @@ export default defineUserConfig({
       { text: '框架文档', link: '/docs/frame-document/' },
       { text: '英文学习', link: '/docs/english-learning/' },
       { text: '拳击训练', link: '/docs/boxing-training/' },
-      {
-        text: 'Docs',
-        children: [
-          { text: 'vuepress-reco', link: '/docs/theme-reco/home' },
-          { text: 'vuepress-theme-reco', link: '/blogs/other/guide' },
-        ],
-      },
     ],
     series: {
       '/docs/theme-reco/': [
@@ -61,5 +72,4 @@ export default defineUserConfig({
       ],
     },
   }),
-  // debug: true,
 })
