736e7d275ad3e23558723fef95b27d2ffff9ccd3
AhJindeg <AhJindeg@gmail.com>
2024-05-05 15:49:26 +0800

    refactor: Simplify VuePress configuration

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,6 +1,5 @@
 import { defineUserConfig } from 'vuepress'
-import recoTheme from 'vuepress-theme-reco'
-import { navbar as recoThemeNavbar, series as recoThemeSeries } from './recoTheme/index.ts'
+import { theme as recoTheme } from './recoTheme/index.ts'
 
 export default defineUserConfig({
   debug: true,
@@ -8,22 +7,5 @@ export default defineUserConfig({
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
   title: "AhJindeg's Blog",
   description: "AhJindeg's Blog, AhJindeg, Arthur, Arthur AJ. W., 阿烬°, 前端技术博客",
-  theme: recoTheme({
-    style: '@vuepress-reco/style-default',
-    author: 'AhJindeg',
-    authorAvatar: '/head.png',
-    home: '/',
-    logo: '/logo.png',
-    docsRepo: 'https://github.com/AhJindeg/AhJindeg.github.io.git',
-    docsBranch: 'master',
-    docsDir: '',
-    lastUpdatedText: '',
-    editLink: false,
-    navbar: [
-      ...recoThemeNavbar
-    ],
-    series: {
-      ...recoThemeSeries
-    }
-  }),
+  theme: recoTheme
 })
--- .vuepress/recoTheme/index.ts
+++ .vuepress/recoTheme/index.ts
@@ -1,4 +1,19 @@
+import recoTheme from 'vuepress-theme-reco'
 import navbar from './navbar/index.ts'
 import series from './series/index.ts'
 
-export { navbar, series }
+export const theme = recoTheme({
+  style: '@vuepress-reco/style-default',
+  colorMode: 'dark',
+  author: 'AhJindeg',
+  authorAvatar: '/head.png',
+  home: '/',
+  logo: '/logo.png',
+  docsRepo: 'https://github.com/AhJindeg/AhJindeg.github.io.git',
+  docsBranch: 'master',
+  docsDir: '',
+  lastUpdatedText: '',
+  editLink: false,
+  navbar,
+  series
+})
